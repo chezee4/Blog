48e411d9f3b90f526dab26e81350f28c3ca308db
chezee4 <vadimdufinec@gmail.com>
2023-06-22 21:59:19 +0300

    refactoring

--- js/script.js
+++ js/script.js
@@ -1,14 +1,10 @@
 const menuBurger = document.querySelector(".header__menu-burger");
-const menuElements = document.querySelectorAll(
-  ".header__menu-burger,.header__menu"
-);
+const headerMenu = document.querySelector(".header__menu");
 
 
 /*--------------------------burger--------------------------*/
 
 menuBurger.addEventListener("click", () => {
-  if (window.innerWidth <= 1110) {
-    menuElements.forEach((element) => element.classList.toggle("active"));
-  }
- 
+    menuBurger.classList.toggle("active");
+    headerMenu.classList.toggle("active");
 });
